cc808cd77287353b267757c684bc44f91b3569a5
Yuhn-z <1029363172@qq.com>
2020-05-01 01:45:46 +0800

    modify some docs

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/">基础</router-link> |
+      <router-link to="/about">数据</router-link>
     </div>
     <router-view />
   </div>
